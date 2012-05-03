9a354c188934167d322f4db7340c2628dff8e4fd
Limin Fu <phoolimin@gmail.com>
2012-05-03 16:22:32 -0700

    Add: a few minor changes and fixings to conform to debian packaging rules.

--- cd-hit-para.pl
+++ cd-hit-para.pl
@@ -52,7 +52,7 @@ while ($arg=shift) {
   elsif ($arg eq "--Q") { $queue      = shift; }
   elsif ($arg eq "--T") { $queue_type = shift; }
   elsif ($arg eq "--R") { $restart_in = shift; }
-  else  {$arg_pass       .= " $arg " . shift; }
+  else  {$arg_pass       .= " $arg "; }
 }
 ($in and $out) || print_usage();
 
--- psi-cd-hit.pl
+++ psi-cd-hit.pl
@@ -1,5 +1,10 @@
 #!/usr/bin/perl -w
 
+package PSI_CDHIT;
+
+use strict;
+no warnings qw(once);
+
 our $script_name = $0;
 our $script_dir = $0;
    $script_dir =~ s/[^\/]+$//;
@@ -7,7 +12,7 @@ our $script_dir = $0;
    $script_dir = "./" unless ($script_dir);
 require "$script_dir/psi-cd-hit-local.pl";
 
-parse_para_etc(@ARGV);
+PSI_CDHIT_LOCAL::parse_para_etc(@ARGV);
 open_LOG();
 
 our @seqs    = ();
@@ -28,20 +33,20 @@ our $NR_passed = 0;
 our $formatdb_no = $NR_no;;
 
 @NR_idx = (0..($NR_no-1));
-@NR_idx = sort { $lens[$b] <=> $lens[$a] } @NR_idx unless (-e $restart_in);
+@NR_idx = sort { $lens[$b] <=> $lens[$a] } @NR_idx unless (-e $PSI_CDHIT_LOCAL::restart_in);
 start_deamon_master();
 
 our $NR90_no = 0;
 our @NR90_seq = ();
 
 $i0 = 0;
-if ( -e $restart_in) { $i0 = read_restart(); } ##  restart after crash
+if ( -e $PSI_CDHIT_LOCAL::restart_in) { $i0 = read_restart(); } ##  restart after crash
 
 blast_formatdb();
 for (; $i0<$NR_no; $i0++) {
   $i = $NR_idx[$i0];
-  my $fout = ($host_no>0) ? "$bl_dir/$i.out" : "$bl_dir/$i";
-  if ( ($host_no>0)     and (not (-e $fout)) and (not $passeds[$i] )) {
+  my $fout = ($PSI_CDHIT_LOCAL::host_no>0) ? "$PSI_CDHIT_LOCAL::bl_dir/$i.out" : "$PSI_CDHIT_LOCAL::bl_dir/$i";
+  if ( ($PSI_CDHIT_LOCAL::host_no>0)     and (not (-e $fout)) and (not $passeds[$i] )) {
     run_batch_blast3($i0) unless ($in_bg[$i]);
   }
 
@@ -56,20 +61,22 @@ for (; $i0<$NR_no; $i0++) {
     $NR90_no++;
   }
 
-  $seqs[$i] = "" unless ($print_db);
+  $seqs[$i] = "" unless ($PSI_CDHIT_LOCAL::print_db);
 
   watch_progress($i0, $NR90_no, $NR_passed, $NR_no, 0);
 
-  if (($i0+1) % $restart_seg == 0 ) { 
+  if (($i0+1) % $PSI_CDHIT_LOCAL::restart_seg == 0 ) { 
     write_restart(); write_db_clstr(); remove_raw_blout($i0);
   }
-  if ($formatdb_no - ($NR_no-$NR_passed) >= $reformat_seg) {blast_formatdb(); }
+  if ($formatdb_no - ($NR_no-$NR_passed) >= $PSI_CDHIT_LOCAL::reformat_seg) {blast_formatdb(); }
+  my $dummy1 = $PSI_CDHIT_LOCAL::restart_seg;
+  my $dummy2 = $PSI_CDHIT_LOCAL::reformat_seg;
 }
 ## END for ($i=0; $i<$NR_no; $i++)
 watch_progress($NR_no-1, $NR90_no, $NR_passed, $NR_no, 1);
 
-if ($print_db) {
-  open(DBOUT, "> $db_out")   || die "Can not write $db_out";
+if ($PSI_CDHIT_LOCAL::print_db) {
+  open(DBOUT, "> $PSI_CDHIT_LOCAL::db_out")   || die "Can not write $PSI_CDHIT_LOCAL::db_out";
   for ($i=0; $i<$NR_no; $i++) {
     next unless ($idens[$i] eq "*");
     my $seq = $seqs[$i];
