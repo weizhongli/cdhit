85914cef004307272e8c4d8452d35e51ef871026
liwz <liwz@CentOS-vm.(none)>
2017-06-02 10:54:32 -0700

    NG-Omics-Miseq-16S.pl     v2

--- usecases/Miseq-16S/NG-Omics-Miseq-16S.pl
+++ usecases/Miseq-16S/NG-Omics-Miseq-16S.pl
@@ -43,7 +43,7 @@ $NGS_batch_jobs{"qc"} = {
   "cores_per_cmd"     => 4,                     # number of threads used by command below
   "no_parallel"       => 1,                     # number of total jobs to run using command below
   "command"           => <<EOD,
-java -jar NGS_prog_trimmomatic PE -threads 4 -phred33 \\DATA.0 \\DATA.1 \\SELF/R1.fq \\SELF/R1-s.fq \\SELF/R2.fq \\SELF/R2-s.fq \\
+java -jar $NGS_prog_trimmomatic PE -threads 4 -phred33 \\DATA.0 \\DATA.1 \\SELF/R1.fq \\SELF/R1-s.fq \\SELF/R2.fq \\SELF/R2-s.fq \\
     SLIDINGWINDOW:4:20 LEADING:3 TRAILING:3 MINLEN:\\CMDOPTS.0 MAXINFO:80:0.5 1>\\SELF/qc.stdout 2>\\SELF/qc.stderr
 
 perl -e '\$i=0; while(<>){ if (/^\@/) {\$i++;  print ">Sample|\\SAMPLE|\$i ", substr(\$_,1); \$a=<>; print \$a; \$a=<>; \$a=<>;}}' < \\SELF/R1.fq > \\SELF/R1.fa &
@@ -54,56 +54,8 @@ rm -f \\SELF/R1.fq \\SELF/R2.fq \\SELF/R1-s.fq \\SELF/R2-s.fq
 EOD
 };
 
+
 $NGS_batch_jobs{"otu"} = {
-  "injobs"            => ["qc"],
-  "CMD_opts"          => ["150", "100", "0.97", "0.0001", "path_to_spliced_ref_db-R1", "path_to_spliced_ref_db-R1", "75"],
-  "execution"         => "sh_1",               # where to execute
-  "cores_per_cmd"     => 2,                    # number of threads used by command below
-  "no_parallel"       => 1,                    # number of total jobs to run using command below
-  "command"           => <<EOD,
-$CD_HIT_dir/cd-hit-est -i \\INJOBS.0/R1.fa -j \\INJOBS.0/R2.fa -o \\SELF/seq.nr -op \\SELF/seq.nr.2 -sf 1 -sc 1 -P 1 -r 0 \\
-    -cx \\CMDOPTS.0 -cy \\CMDOPTS.1 -c 1.0  -n 10 -G 1 -b 1  -T 1 -M 8000  -d 0 -p 1 > \\SELF/seq.nr.log
-$CD_HIT_dir/cd-hit-est -i \\SELF/seq.nr   -o \\SELF/seq.chimeric-clstr.R1 -r 0 -cx \\CMDOPTS.6 -c 0.99 -n 10 -G 0 -b 1 -A 50 -T 1 -M 8000  -d 0 -p 1 > \\SELF/seq.chimeric-clstr.R1.log
-$CD_HIT_dir/cd-hit-est -i \\SELF/seq.nr.2 -o \\SELF/seq.chimeric-clstr.R2 -r 0 -cx \\CMDOPTS.6 -c 0.99 -n 10 -G 0 -b 1 -A 50 -T 1 -M 8000  -d 0 -p 1 > \\SELF/seq.chimeric-clstr.R2.log
-$CD_HIT_dir/cd-hit-est -i \\SELF/seq.nr -j \\SELF/seq.nr.2 -o \\SELF/seq.99 -op \\SELF/seq.99.2 -P 1 -r 0 \\
-    -cx \\CMDOPTS.0 -cy \\CMDOPTS.1 -c 0.99 -n 10 -G 1 -b 1  -T 1 -M 8000  -d 0 -p 1 > \\SELF/seq.99.log
-$CD_HIT_dir/usecases/Miseq-16S/filter-chimeric-and-small.pl -c \\CMDOPTS.3 -k \\SELF/seq.nr.clstr \\
-    -i \\SELF/seq.chimeric-clstr.R1.clstr -j \\SELF/seq.chimeric-clstr.R2.clstr \\
-    -a \\SELF/seq.99.clstr -f \\SELF/seq.99 -g \\SELF/seq.99.2 -o \\SELF/seq.99f
-mv \\SELF/seq.99f.log \\SELF/removed-chimeric-or-low-abundant-reads.list
-#### Now seq.99f and seq99f.2 are good seqs
-
-cat \\CMDOPTS.4 \\SELF/seq.99f   > \\SELF/seq.99fwref
-cat \\CMDOPTS.5 \\SELF/seq.99f.2 > \\SELF/seq.99fwref.2
-$CD_HIT_dir/cd-hit-est -i \\SELF/seq.99fwref -j \\SELF/seq.99fwref.2 -o \\SELF/seq.97fwref -op \\SELF/seq.97fwref.2 -P 1 -r 0 \\
-    -cx \\CMDOPTS.0 -cy \\CMDOPTS.1 -c 0.97 -n 10 -G 1 -b 10  -T 1 -M 8000  -d 0 -p 1 > \\SELF/seq.97fwref.log
-$CD_HIT_dir/usecases/Miseq-16S/filter-refonly-cluster.pl < \\SELF/seq.97fwref.clstr > \\SELF/seq.97fwref.clstr.2
-mv \\SELF/seq.97fwref.clstr.2 \\SELF/seq.97fwref.clstr
-$CD_HIT_dir/clstr_rev.pl \\SELF/seq.nr.clstr       \\SELF/seq.99f.clstr     > \\SELF/seq.99f-full.clstr
-$CD_HIT_dir/clstr_rev.pl \\SELF/seq.99f-full.clstr \\SELF/seq.97fwref.clstr > \\SELF/seq.97f-full.clstr
-
-#### 2nd round of chimeric checking
-$CD_HIT_dir/cd-hit-est -i \\SELF/seq.99fwref   -o \\SELF/seq.97.chimeric-clstr.R1 -r 0 -cx \\CMDOPTS.6 -c 0.99 -n 10 -G 0 -b 1 -A 50 -T 1 -M 8000  -d 0 -p 1 > \\SELF/seq.97.chimeric-clstr.R1.log
-$CD_HIT_dir/cd-hit-est -i \\SELF/seq.99fwref.2 -o \\SELF/seq.97.chimeric-clstr.R2 -r 0 -cx \\CMDOPTS.6 -c 0.99 -n 10 -G 0 -b 1 -A 50 -T 1 -M 8000  -d 0 -p 1 > \\SELF/seq.97.chimeric-clstr.R2.log
-$CD_HIT_dir/usecases/Miseq-16S/filter-chimeric-by-ref.pl -i \\SELF/seq.97.chimeric-clstr.R1.clstr -j \\SELF/seq.97.chimeric-clstr.R2.clstr \\
-    -a \\SELF/seq.97f-full.clstr -f \\SELF/seq.99f -g \\SELF/seq.99f.2 -o \\SELF/seq.97f-fullf 
-cat \\SELF/seq.97f-fullf.log >> \\SELF/removed-chimeric-or-low-abundant-reads.list
-$CD_HIT_dir/clstr_sort_by.pl < \\SELF/seq.97f-fullf.clstr > \\SELF/OTU.clstr
-#### Now seq.97f-fullf seq.97f-fullf.2 are good seqs
-
-rm -f \\SELF/seq.chimeric-clstr.R1    \\SELF/seq.chimeric-clstr.R1.log    \\SELF/seq.chimeric-clstr.R2    \\SELF/seq.chimeric-clstr.R2.log 
-rm -f \\SELF/seq.97.chimeric-clstr.R1 \\SELF/seq.97.chimeric-clstr.R1.log \\SELF/seq.97.chimeric-clstr.R2 \\SELF/seq.97.chimeric-clstr.R2.log
-rm -f \\SELF/seq.99 \\SELF/seq.99.2
-rm -f \\SELF/seq.97fwref \\SELF/seq.97fwref.2 
-rm -f \\SELF/seq.97f-fullf.log
-# rm -f \\SELF/seq.99f \\SELF/seq.99f.2
-
-EOD
-};
-
-
-
-$NGS_batch_jobs{"otuv2"} = {
   "injobs"            => ["qc"],
   "CMD_opts"          => ["150", "100", "0.97", "0.0001", "path_to_spliced_ref_db-R1", "path_to_spliced_ref_db-R1", "75"],
   "execution"         => "sh_1",               # where to execute
@@ -122,15 +74,39 @@ $CD_HIT_dir/usecases/Miseq-16S/filter-chimeric-and-small.pl -c \\CMDOPTS.3 -k \\
     -i \\SELF/seq.chimeric-clstr.R1.clstr -j \\SELF/seq.chimeric-clstr.R2.clstr \\
     -a \\SELF/seq.99.clstr -f \\SELF/seq.99 -g \\SELF/seq.99.2 -o \\SELF/seq.99f
 $CD_HIT_dir/clstr_rev.pl \\SELF/seq.nr.clstr       \\SELF/seq.99f.clstr     > \\SELF/seq.99f-all.clstr
-$CD_HIT_dir/cd-hit-est -i \\SELF/seq.99 -j \\SELF/seq.99.2 -o \\SELF/seq.97 -op \\SELF/seq.97.2 -P 1 -r 0 \\
+$CD_HIT_dir/cd-hit-est -i \\SELF/seq.99f -j \\SELF/seq.99f.2 -o \\SELF/seq.97 -op \\SELF/seq.97.2 -P 1 -r 0 \\
     -cx \\CMDOPTS.0 -cy \\CMDOPTS.1 -c 0.97 -n 10 -G 1 -b 10  -T 1 -M 8000  -d 0 -p 1 > \\SELF/seq.97.log
 $CD_HIT_dir/cd-hit-est-2d -i \\SELF/seq.97 -j \\SELF/seq.97.2 -i2 \\CMDOPTS.4 -j2 \\CMDOPTS.5 -o \\SELF/seq.97.ref -op \\SELF/seq.97.ref.2 -P 1 -r 0 \\
     -cx \\CMDOPTS.0 -cy \\CMDOPTS.1 -c 0.97 -n 10 -G 1 -b 10  -T 1 -M 8000  -d 0 -p 1 > \\SELF/seq.97.ref.log
-$CD_HIT_dir/clstr_rev.pl \\SELF/seq.99f-all.clstr \\SELF/seq.97.clstr >> \\SELF/seq.97-all.clstr
+$CD_HIT_dir/clstr_rev.pl \\SELF/seq.99f-all.clstr \\SELF/seq.97.clstr > \\SELF/seq.97-all.clstr
 $CD_HIT_dir/clstr_merge.pl \\SELF/seq.97-all.clstr \\SELF/seq.97.ref.clstr > \\SELF/OTU.clstr
+
+rm -f \\SELF/seq.chimeric-clstr.R1    \\SELF/seq.chimeric-clstr.R1.log    \\SELF/seq.chimeric-clstr.R2    \\SELF/seq.chimeric-clstr.R2.log 
+rm -f \\SELF/seq.97.ref \\SELF/seq.97.ref.2 \\SELF/seq.97.ref.log
+mv \\SELF/seq.99f.log \\SELF/chimeric-small-clusters-list.txt
+
 EOD
 };
 
+
+$NGS_batch_jobs{"otu-pooled"} = {
+  "CMD_opts"          => ["150", "100", "0.97", "0.0001", "path_to_spliced_ref_db-R1", "path_to_spliced_ref_db-R1", "75"],
+  "execution"         => "sh_1",               # where to execute
+  "cores_per_cmd"     => 2,                    # number of threads used by command below
+  "no_parallel"       => 1,                    # number of total jobs to run using command below
+  "command"           => <<EOD,
+#### before running
+#### concat seq.99f seq.99f.2 seq.99f-all.clstr chimeric-small-clusters-list.txt
+$CD_HIT_dir/cd-hit-est -i seq.99f -j seq.99f.2 -o seq.97 -op seq.97.2 -P 1 -r 0 \\
+    -cx \\CMDOPTS.0 -cy \\CMDOPTS.1 -c 0.97 -n 10 -G 1 -b 10  -T 1 -M 8000  -d 0 -p 1 > seq.97.log
+$CD_HIT_dir/cd-hit-est-2d -i seq.97 -j seq.97.2 -i2 \\CMDOPTS.4 -j2 \\CMDOPTS.5 -o seq.97.ref -op seq.97.ref.2 -P 1 -r 0 \\
+    -cx \\CMDOPTS.0 -cy \\CMDOPTS.1 -c 0.97 -n 10 -G 1 -b 10  -T 1 -M 8000  -d 0 -p 1 > seq.97.ref.log
+$CD_HIT_dir/clstr_rev.pl seq.99f-all.clstr seq.97.clstr > seq.97-all.clstr
+$CD_HIT_dir/clstr_merge.pl seq.97-all.clstr seq.97.ref.clstr > OTU.clstr
+rm -f seq.97.ref seq.97.ref.2 seq.97.ref.log
+
+EOD
+};
 
 ##############################################################################################
 ########## END
