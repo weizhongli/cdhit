148ca690b1f9dbe4d18c31d0d5d4a90e7b8ffa2b
liwz <liwz@CentOS-vm.(none)>
2017-06-17 13:15:54 -0700

    usecases/Miseq-16S/clstr_2_OTU_table.pl     add new file

---
+++ usecases/Miseq-16S/clstr_2_OTU_table.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl
+#
+use Getopt::Std;
+getopts("s:S:o:f:j:",\%opts);
+
+my $input             = $opts{i}; $input   = "OTU.clstr" unless $input;
+my $output            = $opts{o}; $output  = "OTU.txt" unless ($output);
+my ($i, $j, $k, $str, $cmd, $ll);
+
+my %count = ();
+my %count_t = ();
+my %count_s = ();
+my $OTU_2_ann = ();
+
+open(TMP, $input) || die "can not open $input";
+my $OTU=0;
+while($ll=<TMP>){
+  if ($ll =~ /^>/) {
+    $OTU++;
+  }
+  else {
+    chop($ll);
+    if ($ll =~ /\d+(aa|nt), >(.+)\.\.\./) {
+      my $id = $2;
+      if ($id =~ /^Sample\|([^\|]+)\|/) {
+        $sample_id = $1;
+        $sample_id{$sample_id}=1;
+        $count{$OTU}{$sample_id}++;
+        $count_t{$OTU}++;
+        $count_s{$sample_id}++;
+      }
+      else {
+        $OTU_2_ann{$OTU} = $id;
+      }
+    }
+    else {
+      die "format error $ll";
+    }
+  }
+}
+close(TMP);
+
+my @sample_ids = sort keys %sample_id;
+
+open(OUT1, "> $output") || die "can not write $output";
+print OUT1 "OTU\tAnnotation";
+foreach $sample_id (@sample_ids){
+  print OUT1 "\t$sample_id";
+}
+print OUT1 "\tTotal\n";
+
+for ($i=1; $i<=$OTU; $i++){
+  $ann = "None";
+  if ($OTU_2_ann{$i}) {
+    $ann = $OTU_2_ann{$i};
+  }
+  print OUT1 "OTU",$i, "\t$ann";
+  foreach $sample_id (@sample_ids){
+    $k = $count{$i}{$sample_id}? $count{$i}{$sample_id} : 0;
+    print OUT1 "\t$k";
+  }
+  print OUT1 "\t$count_t{$i}";
+  print OUT1 "\n";
+}
+close(OUT1);
+
+
