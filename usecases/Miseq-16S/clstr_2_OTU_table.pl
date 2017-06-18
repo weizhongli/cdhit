1d007e451be75e567dd5a6ccde8fcce4c1009e64
liwz <liwz@CentOS-vm.(none)>
2017-06-17 22:15:15 -0700

    usecases/Miseq-16S/clstr_2_OTU_table.pl     add taxonomy annotation usecases/Miseq-16S/README     detailed README usecases/Miseq-16S/NG-Omics-Miseq-16S.pl     add clstr_2_OTU_table.pl

--- usecases/Miseq-16S/clstr_2_OTU_table.pl
+++ usecases/Miseq-16S/clstr_2_OTU_table.pl
@@ -11,7 +11,8 @@ my %count = ();
 my %count_t = ();
 my %count_s = ();
 my $OTU_2_ann = ();
-
+my $tree_flag = 0;  #### for greengene header format
+# >4360486|k__Bacteria;.p__Firmicutes;.c__Clostridia;.o__Clostridiales;.f__Lachnospiraceae;.g__Roseburia;.s__faecis
 open(TMP, $input) || die "can not open $input";
 my $OTU=0;
 while($ll=<TMP>){
@@ -31,6 +32,7 @@ while($ll=<TMP>){
       }
       else {
         $OTU_2_ann{$OTU} = $id;
+        $tree_flag = 1 if ($id =~ /\|k__Bacteria;.p__/);
       }
     }
     else {
@@ -47,6 +49,9 @@ print OUT1 "OTU";
 foreach $sample_id (@sample_ids){
   print OUT1 "\t$sample_id";
 }
+if ($tree_flag) {
+  print join("\t", qw/Kingdom Phylum Class Order Family Genus Species/);
+}
 #print OUT1 "\tTotal\n";
 print OUT1 "\tAnnotation\n";
 
@@ -57,6 +62,17 @@ for ($i=1; $i<=$OTU; $i++){
   foreach $sample_id (@sample_ids){
     $k = $count{$i}{$sample_id}? $count{$i}{$sample_id} : 0;
     print OUT1 "\t$k";
+  }
+  if ($tree_flag) {
+    my ($tax_k, $tax_p, $tax_c, $tax_o, $tax_f, $tax_g, $tax_s);
+    if ($ann =~ /k__(\w+)/) {$tax_k = $1} else {$tax_k = "";}
+    if ($ann =~ /p__(\w+)/) {$tax_p = $1} else {$tax_p = "";}
+    if ($ann =~ /c__(\w+)/) {$tax_c = $1} else {$tax_c = "";}
+    if ($ann =~ /o__(\w+)/) {$tax_o = $1} else {$tax_o = "";}
+    if ($ann =~ /f__(\w+)/) {$tax_f = $1} else {$tax_f = "";}
+    if ($ann =~ /g__(\w+)/) {$tax_g = $1} else {$tax_g = "";}
+    if ($ann =~ /s__(\w+)/) {$tax_s = $1} else {$tax_s = "";}
+    print OUT1 join("\t", ($tax_k, $tax_p, $tax_c, $tax_o, $tax_f, $tax_g, $tax_s));
   }
   #print OUT1 "\t$count_t{$i}";
   print OUT1 "\t$ann\n";
